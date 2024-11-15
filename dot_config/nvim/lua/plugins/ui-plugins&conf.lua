49503584bfd78bb73dfbde2e4c113e39c4dac071
JosGuerrero2004 <josiasguerrero2004@gmail.com>
2024-11-15 12:43:55 -0600

    Add ui stuff

---
+++ dot_config/nvim/lua/plugins/ui-plugins&conf.lua
@@ -0,0 +1,15 @@
+return {
+
+  {
+    "HiPhish/rainbow-delimiters.nvim"
+  },
+
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    use_treesitter = true
+  },
+
+  {
+    'DaikyXendo/nvim-material-icon'
+  }
+}
